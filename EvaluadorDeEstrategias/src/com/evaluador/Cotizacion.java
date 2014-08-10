9d69b49020a668f1409b5620c4af0680db26f640
Damián Mazzini <dmazzini@gmail.com>
2014-08-10 14:20:06 -0300

    Cotizacion

---
+++ EvaluadorDeEstrategias/src/com/evaluador/Cotizacion.java
@@ -0,0 +1,17 @@
+package com.evaluador;
+
+import org.joda.time.DateTime;
+
+public class Cotizacion {
+
+	private String accion;
+	private DateTime fecha;
+	private double precio;
+
+	public Cotizacion(String accion, DateTime fecha, double precio) {
+		this.accion = accion;
+		this.fecha = fecha;
+		this.precio = precio;
+	}
+
+}
